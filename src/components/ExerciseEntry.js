3880ceb64b2bba8fb1ad6bc01e257af452c6140f
j3553pat <jessecodedev@gmail.com>
2023-08-07 19:15:54 -0500

    added exercise and home pages. Need to fix bugs and connect API

--- src/components/ExerciseEntry.js
+++ src/components/ExerciseEntry.js
@@ -1,9 +1,60 @@
-import React from 'react'
+import React, { useState } from 'react';
 
-function ExerciseEntry() {
+const ExerciseEntry = () => {
+  const [exerciseName, setExerciseName] = useState('');
+  const [minutes, setMinutes] = useState(0);
+  const [reps, setReps] = useState(0);
+  const [sets, setSets] = useState(0);
+
+  const handleExerciseNameChange = (e) => {
+    setExerciseName(e.target.value);
+  };
+
+  const handleMinutesChange = (e) => {
+    setMinutes(Number(e.target.value));
+  };
+
+  const handleRepsChange = (e) => {
+    setReps(Number(e.target.value));
+  };
+
+  const handleSetsChange = (e) => {
+    setSets(Number(e.target.value));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setExerciseName('');
+    console.log('Exercise Name:', exerciseName);
+    setMinutes(0);
+    console.log('Minutes:', minutes);
+    setReps(0);
+    console.log('Reps:', reps);
+    setSets(0);
+    console.log('Sets:', sets);
+  };
+
   return (
-    <div>ExerciseEntry</div>
-  )
-}
+    <form onSubmit={handleSubmit}>
+      <label>
+        Exercise Name:
+        <input type="text" value={exerciseName} onChange={handleExerciseNameChange} />
+      </label>
+      <label>
+        Minutes:
+        <input type="number" value={minutes} onChange={handleMinutesChange} />
+      </label>
+      <label>
+        Reps:
+        <input type="number" value={reps} onChange={handleRepsChange} />
+      </label>
+      <label>
+        Sets:
+        <input type="number" value={sets} onChange={handleSetsChange} />
+      </label>
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
 
-export default ExerciseEntry
+export default ExerciseEntry;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,36 @@
 import React from 'react'
-import{ Container } from '@mui/material'
 
 function Home() {
   return (
-    <Container>
-    <div>lorem</div>
-    </Container>
+    <div>
+    <header>
+      <h1>Workout Tracker App</h1>
+    </header>
+    <main>
+      <section>
+        <h2>Track Your Workouts</h2>
+        <p>Log your exercises, sets, reps, and durations to keep track of your progress.</p>
+        <Link to="/exerciseentry" className="cta-button">
+          Get Started
+        </Link>
+      </section>
+      <section>
+        <Link to="/progress" className="cta-button">
+          View Progress
+        </Link>
+      </section>
+      <section>
+        <h2>Stay Motivated</h2>
+        <p>Get personalized workout recommendations and set achievable goals.</p>
+        <Link to="/goals" className="cta-button">
+          Set Goals
+        </Link>
+      </section>
+    </main>
+    <footer>
+      <p>&copy; 2023 Workout Tracker App. All rights reserved.</p>
+    </footer>
+  </div>
   )
 }
 
